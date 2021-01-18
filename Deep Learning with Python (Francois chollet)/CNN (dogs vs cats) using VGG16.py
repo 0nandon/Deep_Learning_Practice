4034e0e2e843f883ede5393ca8d9a4e900811c72
onandon <71716834+0nandon@users.noreply.github.com>
2021-01-18 23:53:13 +0900

    Update CNN (dogs vs cats) using VGG16.py

--- Deep Learning with Python (Francois chollet)/CNN (dogs vs cats) using VGG16.py
+++ Deep Learning with Python (Francois chollet)/CNN (dogs vs cats) using VGG16.py
@@ -9,6 +9,8 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.models import load_model
 from tensorflow.keras.applications.vgg16 import VGG16
 
+# imagenetì€ weights ëª¨ë¸ì„ ì´ˆê¸°í™”í•  ê°€ì¤‘ì¹˜ ì²´í¬í¬ì¸íŠ¸ë¥¼ ì§€ì •
+# include_topì€ ë„¤íŠ¸ì›Œí¬ì˜ ìµœìƒìœ„ì¸µ ì™„ì „ ë¶„ë¥˜ê¸°ë¥¼ í¬í•¨í• ì§€, ì•ˆí• ì§€ë¥¼ ì§€ì •
 conv_base = VGG16(weights = 'imagenet', include_top = False, input_shape = (150, 150, 3))
 
 base_dir = '/content/cats_and_dogs_small'
@@ -19,7 +21,7 @@ test_dir = os.path.join(base_dir, 'test')
 datagen = ImageDataGenerator(rescale=1./255)
 batch_size=20
 
-# VGG16 (»çÀü ÈÆ·ÃµÈ ÇÕ¼º°ö Ãş)À» È°¿ëÇØ¼­ ÀÌ¹ÌÁöÀÇ Æ¯¼ºÀ» ÃßÃâÇÑ ÈÄ, µ¥ÀÌÅÍ ¼ÂÀ» ¸®ÅÏÇÑ´Ù.
+# VGG16 (ì‚¬ì „ í›ˆë ¨ëœ í•©ì„±ê³± ì¸µ)ì„ í™œìš©í•´ì„œ ì´ë¯¸ì§€ì˜ íŠ¹ì„±ì„ ì¶”ì¶œí•œ í›„, ë°ì´í„° ì…‹ì„ ë¦¬í„´í•œë‹¤.
 def extract_features(directory, sample_count):
   features = np.zeros(shape=(sample_count, 4, 4, 512))
   labels = np.zeros(shape=(sample_count))
@@ -45,7 +47,7 @@ train_features, train_labels = extract_features(train_dir, 2000)
 validation_features, validation_labels = extract_features(validation_dir, 1000)
 test_features, test_labels = extract_features(test_dir, 1000)
 
-# ºĞ¸® Ãş¿¡ µé¾î°¡±â Àü¿¡ µ¥ÀÌÅÍ¸¦ ÀÏ·Ä·Î ÆîÄ£´Ù. (Flatten() ¿ªÇÒ)
+# ë¶„ë¦¬ ì¸µì— ë“¤ì–´ê°€ê¸° ì „ì— ë°ì´í„°ë¥¼ ì¼ë ¬ë¡œ í¼ì¹œë‹¤. (Flatten() ì—­í• )
 train_features = train_features.reshape(2000, -1)
 validation_features = validation_features.reshape(1000, -1)
 test_features = test_features.reshape(1000, -1)
@@ -65,7 +67,7 @@ model.compile(optimizer = RMSprop(lr=2e-5),
 history = model.fit(train_features, train_labels,
                     epochs = 30,
                     batch_size = 20,
-                    validation_data = (validation_features, validation_labels)ÇÒ
+                    validation_data = (validation_features, validation_labels)í• 
 
 acc = history.history['acc']
 val_acc = history.history['val_acc']
