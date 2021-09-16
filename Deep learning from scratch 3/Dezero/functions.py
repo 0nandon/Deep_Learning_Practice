87c1dbe95b38fd060ef152e5ec689d8d0e2e8ca3
onandon <71716834+0nandon@users.noreply.github.com>
2021-09-17 00:10:44 +0900

    Update functions.py

--- Deep learning from scratch 3/Dezero/functions.py
+++ Deep learning from scratch 3/Dezero/functions.py
@@ -2,25 +2,25 @@ import numpy as np
 from dezero import Function
 
 class Sin(Function):
-	def forward(self, x):
-		y = np.sin(x)
-		return y
+    def forward(self, x):
+        y = np.sin(x)
+	return y
 
-	def backward(self, gy):
-		x, = self.inputs
-		gx = gy * cos(x)
-		return gx
+    def backward(self, gy):
+	x, = self.inputs
+	gx = gy * cos(x)
+	return gx
 	
 
 class Cos(Function):
     def forward(self, x):
-		y = np.cos(x)
-		return y
+	y = np.cos(x)
+	return y
 
     def backward(self, gy):
-		x, = self.inputs
-		gx = gy * -sin(x)
-		return gx
+	x, = self.inputs
+	gx = gy * -sin(x)
+	return gx
 	
 
 def sin(x):
@@ -28,6 +28,6 @@ def sin(x):
 	
 
 def cos(x):
-	return Cos()(x)
+    return Cos()(x)
 	
 
