e7b554d0a7d70eb5025d545f1e3daae9caa75f39
onandon <71716834+0nandon@users.noreply.github.com>
2021-09-19 00:19:30 +0900

    Update functions.py

--- Deep learning from scratch 3/Dezero/functions.py
+++ Deep learning from scratch 3/Dezero/functions.py
@@ -1,33 +1,171 @@
 import numpy as np
-from dezero import Function
+from dezero.core import Function, Variable, as_variable, as_array
+from dezero import utils
 
+# sin
 class Sin(Function):
     def forward(self, x):
-        y = np.sin(x)
-	return y
+    	y = np.sin(x)
+        return y
 
     def backward(self, gy):
-	x, = self.inputs
-	gx = gy * cos(x)
-	return gx
+    	x, = self.inputs
+        gx = gy * cos(x)
+        return gx
 	
-
+# cos
 class Cos(Function):
     def forward(self, x):
-	y = np.cos(x)
-	return y
+        y = np.cos(x)
+        return y
 
     def backward(self, gy):
-	x, = self.inputs
-	gx = gy * -sin(x)
-	return gx
-	
+        x, = self.inputs
+        gx = gy * -sin(x)
+        return gx
+
+# tanh
+class Tanh(Function):
+    def forward(self, x):
+        y = np.tanh(x)
+        return y
+
+    def backward(self, gy):
+        y = self.outputs[0]()
+        gx = gy * (1 - y * y)
+        return gx
+
+# reshape
+class Reshape(Function):
+    def __init__(self, shape):
+        self.shape = shape
+        super().__init__()
+
+    def forward(self, x):
+        self.x_shape = x.shape
+        y = x.reshape(self.shape)
+        return y
+
+    def backward(self, gy):
+        return reshape(gy, self.x_shape)
+
+
+# transpose
+class Transpose(Function):
+    def __init__(self, axes=None):
+        self.axes = axes
+        super().__init__()
+
+    def forward(self, x):
+        y = x.transpose(self.axes)
+        return y
+
+    def backward(self, gy):
+        if self.axes is None:
+            return transpose(gy)
+        
+        axes_len = len(self.axes)
+        inv_axes = tuple(np.argsort([ax % axes_len for ax in self.axes]))
+        gx = transpose(gy, inv_axes)
+        return gx
+
+# sum
+class Sum(Function):
+    def __init__(self, axis, keepdims):
+        self.axis = axis
+        self.keepdims = keepdims
+        super().__init__()
+
+    def forward(self, x):
+        self.x_shape = x.shape
+        y = x.sum(axis=self.axis, keepdims=self.keepdims)
+        return y
+
+    def backward(self, gy):
+        gy = utils.reshape_sum_backward(gy, self.x_shape, self.axis, self.keepdims)
+        gx = broadcast_to(gy, self.x.shape)
+        return gx
+
+# 브로드 캐스트
+class BroadcastTo(Function):
+    def __init__(self, shape):
+        self.shape = shape
+        super().__init__()
+
+    def forward(self, x):
+        self.x_shape = x.shape
+        y = np.broadcast_to(x, self.shape)
+        return y
+
+    def backward(self, gy):
+        gx = sum_to(gy, self.x_shape)
+        return gx
+
+
+class SumTo(Function):
+    def __init__(self, shape):
+        self.shape = shape
+        super().__init__()
+
+    def forward(self, x):
+        self.x_shape = x.shape
+        y = utils.sum_to(x, self.shape)
+        return y
+
+    def backward(self, gy):
+        gx = broadcast_to(gy, self.x_shape)
+        return gx
+
+
+class MatMul(Function):
+    def forward(self, x, W):
+        y = np.dot(x, W)
+        return y
+
+    def backward(self, gy):
+        x, W = self.inputs
+        gx = matmul(gy, W.T)
+        gW = matmul(x.T, gy)
+        return gx, gW
+
 
 def sin(x):
     return Sin()(x)
 	
 
 def cos(x):
-    return Cos()(x)
-	
+	  return Cos()(x)
 
+
+def tanh(x):
+    return Tanh()(x)
+
+
+def reshape(x, shape):
+    if x.shape == shape:
+        return as_variable(x)
+    return Reshape(shape)(x)
+
+
+def transpose(x, axes=None):
+    return Transpose(axes)(x)
+
+
+def sum(x, axis=None, keepdims=False):
+    return Sum(axis, keepdims)(x)
+
+
+def broadcast_to(x, shape):
+    if x.shape == shape:
+        return as_variable(x)
+    return BroadcastTo(shape)(x)
+
+
+def sum_to(x, shape):
+    if x.shape == shape:
+        return as_variable(x)
+    return SumTo(shape)(x)
+
+
+def matmul(x, W):
+    return MatMul()(x, W)
