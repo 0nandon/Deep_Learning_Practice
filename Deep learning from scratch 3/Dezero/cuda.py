f8a3a6b60100a699e7296187e3fe1c9217bc9416
onandon <71716834+0nandon@users.noreply.github.com>
2021-09-20 21:09:23 +0900

    Create cuda.py

---
+++ Deep learning from scratch 3/Dezero/cuda.py
@@ -0,0 +1,23 @@
+import numpy as np
+gpu_enable = True
+
+try:
+    import cupy as cp
+    cupy = cp
+except:
+    gpu_enable = False
+from dezero import Variable
+
+
+def get_array_module(x):
+    if isinstance(x, Variable):
+        x = x.data
+        
+    if not gpu_enable:
+        return np
+      
+    xp = cp.get_array_module(x)
+    return xp
+  
+
+  
